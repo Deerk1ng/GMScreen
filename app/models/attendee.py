0a9858c7a5a5c9f3ccf5eb9c4886b78c6cff35e9
Hayden Ruiz <haydenr1111@gmail.com>
2024-10-17 23:40:43 -0700

    started attendee models

---
+++ app/models/attendee.py
@@ -0,0 +1,26 @@
+#attending model
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Attendee(db.Model):
+    __tablename__ = 'attendee'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id"), ondelete="CASCADE"), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("events.id"), ondelete="CASCADE"), nullable=False)
+    is_owner = db.Column(db.Boolean, default=False)
+    status = db.Column(db.String, nullable=False)
+
+    user = db.relationship("User", back_populates = "event_attending")
+    event = db.relationship("Event", back_populates = "attendees")
+
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'user_id' : self.user_id,
+            'event_id' : self.event_id,
+            'is_owner' : self.is_owner
+        }
