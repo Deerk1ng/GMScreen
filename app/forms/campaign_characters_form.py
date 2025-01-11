3856b59033820a67d3e539da84a5f628bf008fb4
Hayden Ruiz <haydenr1111@gmail.com>
2025-01-11 12:55:21 -0800

    create campaign route

--- app/forms/campaign_characters_form.py
+++ app/forms/campaign_characters_form.py
@@ -2,6 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError
 
-class CreateCampaignForm(FlaskForm):
+class CreateCampaignCharacterForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     description = StringField('description')
