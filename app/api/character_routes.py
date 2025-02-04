68cf7686d86c538b6423a0367a1504bf7a2a670d
Hayden Ruiz <haydenr1111@gmail.com>
2025-02-03 22:00:54 -0800

    attempt 3

--- app/api/character_routes.py
+++ app/api/character_routes.py
@@ -1,7 +1,7 @@
 # routes
 from flask import Blueprint, jsonify, redirect, request
 from app import db
-from app.models import Character
+from app.models import Character, Campaign
 from flask_login import current_user, login_required
 #import forms
 from app.forms import CreateCharacterForm
