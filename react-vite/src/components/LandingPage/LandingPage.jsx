f842dd495437f3e476be6230fa3e4091ca9452cd
Hayden Ruiz <haydenr1111@gmail.com>
2025-02-27 21:46:55 -0800

    changing landing apge

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -17,9 +17,19 @@ const LandingPage = () => {
     return (
         <div className='main-div land-main'>
             <h1 className='third-color'>Welcome to Guild Meet</h1>
-            <div className='land-desc'>Guild meet is a website designed to make planning for your nerdy events easy. Schedule events that your group mates can sign up to attend. In the future, campaign groups will be implemented, allowing for a more exclusive membership status and for private usage of the schedule feature. </div>
+            <div>
+                <div className='land-desc'>Guild meet is a website designed to make planning for your nerdy events easy. Schedule events that your group mates can sign up to attend. In the future, campaign groups will be implemented, allowing for a more exclusive membership status and for private usage of the schedule feature. </div>
+                <OpenModalButton
+                    buttonText="Sign Up"
+                    modalComponent={<SignupFormModal />}
+                />
+                <OpenModalButton
+                    buttonText="Log in or Use A Demo account"
+                    modalComponent={<LoginFormModal />}
+                />
+            </div>
             <div className='land-mod'>
-                <h2 className='land-head first-color'>Register Today</h2>
+                <h2 className='land-head'>Register Today</h2>
                 <OpenModalButton
                     buttonText="Sign Up"
                     modalComponent={<SignupFormModal />}
