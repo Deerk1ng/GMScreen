398f6e750b1987fe0820e7c76ce1ba19fe5b4afa
Hayden Ruiz <haydenr1111@gmail.com>
2025-03-04 19:18:23 -0800

    continued work on landing page

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -18,7 +18,7 @@ const LandingPage = () => {
         <div className='main-div land-main'>
             <h1 className='third-color'>Welcome to Guild Meet</h1>
             <div>
-                <div className='land-desc'>Guild meet is a website designed to make planning for your nerdy events easy.</div>
+                <div className='land-main-desc'>Guild meet is a website designed to make planning for your nerdy events easy.</div>
                 <OpenModalButton
                     buttonText="Sign Up"
                     modalComponent={<SignupFormModal />}
@@ -28,13 +28,16 @@ const LandingPage = () => {
                     modalComponent={<LoginFormModal />}
                 />
             </div>
-            <div>
-                <div className='land-desc'>Create a Campaign</div>
-                <div>Create campaigns for your players to join</div>
-                <div className='land-desc'>Join your friends.</div>
-                <div>Create characters and join other's campaigns as you see fit</div>
-                <div className='land-desc'>Schedule events.</div>
-                <div>Plan events or choose to attend any events available to your campaign</div>
+            <div className='land-triple'>
+                <div className='land-desc'>Create a Campaign
+                    <div>Create campaigns for your players to join</div>
+                </div>
+                <div className='land-desc'>Join your friends.
+                    <div>Create characters and join other's campaigns as you see fit</div>
+                </div>
+                <div className='land-desc'>Schedule events.
+                  <div>Plan events or choose to attend any events available to your campaign</div>
+                </div>
             </div>
             <div className='land-mod'>
                 <h2 className='land-head'>Register Today</h2>
