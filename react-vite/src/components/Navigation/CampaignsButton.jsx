fd05936e451ad21ef8df997a9fbd73f646d01175
Hayden Ruiz <haydenr1111@gmail.com>
2025-05-26 18:24:17 -0700

    adding campaign button

---
+++ react-vite/src/components/Navigation/CampaignsButton.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FaDungeon } from "react-icons/fa";
+import OpenModalMenuItem from "./OpenModalMenuItem";
+import { useNavigate } from "react-router-dom";
+
+function CampaignsButton() {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const user = useSelector((store) => store.session.user);
+  const ulRef = useRef();
+  const navigate = useNavigate()
+
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const closeMenu = () => setShowMenu(false);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(thunkLogout())
+    .then(() => navigate('/'));
+  };
+
+  return (
+    <>
+      <button id='profile-btn' onClick={toggleMenu}>
+        <FaDungeon className="pfp" />
+      </button>
+      {showMenu && (
+        <ul className={"profile-dropdown"} ref={ulRef}>
+          {user ? (
+            <>
+              <li className="user-name">{user.username}</li>
+              <li>{user.email}</li>
+              <li>
+                <button className="button" onClick={logout}>Log Out</button>
+              </li>
+            </>
+          ) : (
+            <div className="profile-items">
+              <OpenModalMenuItem
+                itemText="Log In"
+                onItemClick={closeMenu}
+                modalComponent={<LoginFormModal />}
+              />
+              <OpenModalMenuItem
+                itemText="Sign Up"
+                onItemClick={closeMenu}
+                modalComponent={<SignupFormModal />}
+              />
+            </div>
+          )}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default CampaignsButton;
